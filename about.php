fefd047236c626f1c2f3f0ba7fadc4fee3bf5f61
Everts <101229677+warmjuly@users.noreply.github.com>
2022-04-22 07:40:16 +0300

    Update about.php

--- about.php
+++ about.php
@@ -3,6 +3,6 @@
 
     <h2>About</h2>
 
-    <p class="text-center">This is a feedback app created with PHP, MYSQL and Bootstrap. Feel free to <a href="index.php">submit</a> an entry and view it on the <a href="feedback.php">feedback</a> page if you haven't already.</p>
+    <p class="text-center">This is a feedback app created with PHP, MySQL and Bootstrap. Feel free to <a href="index.php">submit</a> an entry and view it on the <a href="feedback.php">feedback</a> page if you haven't already.</p>
 
 <?php include "inc/footer.php"; ?>
